02491634ec18ff2198117f2a51c45cb1cfa639c0
林彥妤 <stephanieyenyu@gmail.com>
2026-06-29 15:12:07 +0800

    Update PartAExporter.cs

--- csharp_server/PartAExporter.cs
+++ csharp_server/PartAExporter.cs
@@ -9,7 +9,7 @@ public static class PartAExporter
         string imagePath = "images/test_scene.jpg";
 
         // useWebcam = true 會從攝影機拍；false 用現有圖片
-        bool useWebcam = false;
+        bool useWebcam = true;
         int cameraIndex = 0;
 
         Console.WriteLine("Current folder: " + Directory.GetCurrentDirectory());
